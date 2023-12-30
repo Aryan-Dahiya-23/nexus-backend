009d9f8d8ed6593c018ed6e504785a59dda8686f
unknown <aryan23dahiya@gmail.com>
2023-12-30 19:02:59 +0530

    same site

--- index.js
+++ index.js
@@ -32,9 +32,12 @@ app.use(
         saveUninitialized: false,
 
         cookie: {
-            secure: process.env.NODE_ENV === "development" ? false : true,
-            httpOnly: process.env.NODE_ENV === "development" ? false : true,
-            sameSite: process.env.NODE_ENV === "development" ? false : "none",
+            // secure: process.env.NODE_ENV === "development" ? false : true,
+            // httpOnly: process.env.NODE_ENV === "development" ? false : true,
+            // sameSite: process.env.NODE_ENV === "development" ? false : "none",
+            secure: true,
+            httpOnly:  true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
         },
     })
